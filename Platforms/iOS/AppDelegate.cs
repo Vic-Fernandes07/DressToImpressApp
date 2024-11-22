dac0890b0d895b78202085ecc7b1f2c32a5b9d81
Vic-Ronchi <vitoria.ronchi7@gmail.com>
2024-11-22 08:28:05 -0300

    Adicionar arquivos de projeto.

---
+++ Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace DressToImpress
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
